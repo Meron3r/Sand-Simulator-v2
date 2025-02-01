18a72ae79f75b5c3f3775e6e9c322ace7dadf463
Meron3r <mustaforov@gmail.com>
2025-02-01 18:36:20 +0000

    Quality of life improvements.

--- main.cpp
+++ main.cpp
@@ -37,6 +37,8 @@ typedef struct cell_t
     u32 weigh;
     u32 flamibility = 0;
     u32 fuel = 0;
+    u32 friction = 1;
+    bool isFreeFalling = true;
 } cell_t;
 
 // MouseCell and Game Grid
@@ -45,7 +47,7 @@ cell_t grid[HEIGHT][WIDTH];
 
 // Pre-set types of Cells
 #define AIR cell_t {0, -100, sf::Color::Black, false, 0}
-#define SAND cell_t {1, -100, (dis(gen) & 1 ? sf::Color::Yellow : sf::Color(255, 255, 150)), false, 2}
+#define SAND cell_t {1, -100, (dis(gen) & 1 ? sf::Color::Yellow : sf::Color(255, 255, 150)), false, 2, 10}
 #define WATER cell_t {2, -100, sf::Color::Blue, false, 1}
 #define STONE cell_t {3, -100, sf::Color(169, 169, 169), false, 10}
 #define FIRE cell_t {4, 15, (dis(gen) & 1 ? sf::Color::Red : sf::Color (255, 165, 0)), false, 0}
@@ -110,9 +112,17 @@ bool isWithinBounds(u32 x, u32 y)
 void updateSand(u32 i, u32 j)
 {
     grid[i][j].updated = true;
+
+    if (grid[i][j].isFreeFalling && isWithinBounds(i, j - 1) && dis(gen) % grid[i][j - 1].friction == 0)
+        grid[i][j - 1].isFreeFalling = true;
+    if (grid[i][j].isFreeFalling && isWithinBounds(i, j + 1) && dis(gen) % grid[i][j + 1].friction == 0)
+        grid[i][j + 1].isFreeFalling = true; 
     
     if (isWithinBounds(i + 1, j) && grid[i + 1][j].weigh < grid[i][j].weigh)
         swapCells(i, j, i + 1, j);
+    
+    else if (!grid[i][j].isFreeFalling)
+        return;
     
     else if (isWithinBounds(i + 1, j + 1) && isWithinBounds(i + 1, j - 1) && grid[i + 1][j + 1].weigh < grid[i][j].weigh && grid[i + 1][j - 1].weigh < grid[i][j].weigh)
     {
@@ -131,6 +141,9 @@ void updateSand(u32 i, u32 j)
         swapCells(i, j, i + 1, j + 1);
     else if (isWithinBounds(i + 1, j - 1) && grid[i + 1][j - 1].weigh < grid[i][j].weigh)
         swapCells(i, j, i + 1, j - 1);
+
+    else
+        grid[i][j].isFreeFalling = false;
 }
 
 void updateWater(u32 i, u32 j)
@@ -300,6 +313,60 @@ void updateElements(u32 i, u32 j)
     }
 }
 
+void drawUI()
+{
+    const int buttonSize = 40;
+    const int padding = 10;
+    const int startX = 10;
+    const int startY = 10;
+
+    sf::RectangleShape button(sf::Vector2f(buttonSize, buttonSize));
+
+    sf::Color colors[BRUSH_LIMIT] = 
+    {
+        sf::Color::Black, // AIR
+        sf::Color::Yellow, // SAND
+        sf::Color::Blue, // WATER
+        sf::Color(169, 169, 169), // STONE
+        sf::Color::Red, // FIRE
+        sf::Color(155, 103, 60), // WOOD
+        sf::Color(150, 150, 150), // GUNPOWDER
+        sf::Color(20, 20, 20), // COAL
+        sf::Color(255, 128, 0) // LAVA
+    };
+
+    for (int i = 0; i < BRUSH_LIMIT; ++i)
+    {
+        button.setPosition(startX + i * (buttonSize + padding), startY);
+        button.setFillColor(colors[i]);
+        button.setOutlineThickness(1);
+        button.setOutlineColor(sf::Color::White); // Set the outline color to white
+        window.draw(button);
+    }
+}
+
+void handleUIClick(int mouseX, int mouseY)
+{
+    const int buttonSize = 40;
+    const int padding = 10;
+    const int startX = 10;
+    const int startY = 10;
+
+    if (mouseY >= startY && mouseY <= startY + buttonSize)
+    {
+        for (int i = 0; i < BRUSH_LIMIT; ++i)
+        {
+            int buttonX = startX + i * (buttonSize + padding);
+            if (mouseX >= buttonX && mouseX <= buttonX + buttonSize)
+            {
+                mouseType = i;
+                chooseBrushElement();
+                return; // Exit after handling UI click
+            }
+        }
+    }
+}
+
 void init()
 {
     // Set every cell to AIR
@@ -308,7 +375,7 @@ void init()
             grid[i][j] = AIR;
     
     // Set framerate limit
-    window.setFramerateLimit(80);
+    //window.setFramerateLimit(80);
 
     // Set variables
     mouseType = 0;
@@ -319,23 +386,52 @@ void render()
 {
     // Clear the screen
     window.clear(sf::Color::Black);
-    
-    // Render Cells
-    sf::RectangleShape temp(sf::Vector2f (PIX_SIZE, PIX_SIZE));
+
+    // Create a texture to represent the grid
+    sf::RenderTexture renderTexture;
+    renderTexture.create(WIDTH * PIX_SIZE, HEIGHT * PIX_SIZE);
+    renderTexture.clear(sf::Color::Black);
+
+    // Render Cells onto the texture
+    sf::RectangleShape temp(sf::Vector2f(PIX_SIZE, PIX_SIZE));
     for (u32 i = 0; i < HEIGHT; i++)
     {
         for (u32 j = 0; j < WIDTH; j++)
         {
-            temp.setPosition(j * PIX_SIZE, i * PIX_SIZE);
-            temp.setFillColor(grid[i][j].color);
-
             if (grid[i][j].id == 0)
                 continue;
 
-            window.draw(temp);
+            temp.setPosition(j * PIX_SIZE, i * PIX_SIZE);
+            temp.setFillColor(grid[i][j].color);
+            renderTexture.draw(temp);
         }
     }
-    
+
+    // Finalize the texture
+    renderTexture.display();
+
+    // Draw the texture to the window
+    sf::Sprite sprite(renderTexture.getTexture());
+    window.draw(sprite);
+
+    // Draw the outline for the brush
+    if (!buttonPressed)
+    {
+        int mouseX = sf::Mouse::getPosition(window).x / PIX_SIZE;
+        int mouseY = sf::Mouse::getPosition(window).y / PIX_SIZE;
+
+        sf::RectangleShape outline(sf::Vector2f(BRUSH_SIZE * PIX_SIZE, BRUSH_SIZE * PIX_SIZE));
+        outline.setPosition((mouseX - BRUSH_SIZE / 2) * PIX_SIZE, (mouseY - BRUSH_SIZE / 2) * PIX_SIZE);
+        outline.setFillColor(sf::Color::Transparent);
+        outline.setOutlineColor(sf::Color::White);
+        outline.setOutlineThickness(1);
+
+        window.draw(outline);
+    }
+
+    // Draw the UI
+    drawUI();
+
     // Display to the window
     window.display();
 }
@@ -394,14 +490,22 @@ void keys(sf::Event event)
     // Check if they close the program
     if (event.type == sf::Event::Closed)
         window.close();
-    
+
     if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
-        buttonPressed = true;
+    {
+        // Check if the click is within the UI area
+        int mouseX = event.mouseButton.x;
+        int mouseY = event.mouseButton.y;
+        const int buttonSize = 40;
+        const int startY = 10;
+
+        if (mouseY >= startY && mouseY <= startY + buttonSize)
+            handleUIClick(mouseX, mouseY);
+        else
+            buttonPressed = true; // Only set buttonPressed if the click is outside the UI
+    }
     if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left)
         buttonPressed = false;
-
-    if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Right)
-        mouseType = (mouseType + 1) % BRUSH_LIMIT;
 
     if (event.type == sf::Event::KeyPressed)
     {
